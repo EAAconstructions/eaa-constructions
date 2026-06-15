65cacb81d3380be8f7100dc5dae6b31d16d74410
Davit Grigoryan <d.grigoryan2001@mail.ru>
2026-06-15 02:21:10 -0700

    Update site

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,4 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: "/",
-<<<<<<< HEAD
 })
-=======
-})
->>>>>>> cb8d34b9a2dde031f9813b6b15c8dd445a9191f5
